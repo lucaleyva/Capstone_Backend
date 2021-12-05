8e43c93f155eaeda0d6cf0f9c8af6abb208c5685
Luca Leyva <imlucaleyva@gmail.com>
2021-12-05 10:47:56 -0500

    Setup backend and API with dummy data.

---
+++ backend/base/products.py
@@ -0,0 +1,67 @@
+products = [
+    {
+        '_id': '1',
+        'name': 'Introduction to Agile Software Development',
+        'image': '/images/intro_to_agile.jpg',
+        'description':
+        'Learn why and when you should use agile, how to be agile, \n understanding and applying Extreme Programming (XP) practices, \n collaborating, releasing, planning, developing, process \n improvement, identify and elimante waste, and how to seek \n techincal excellence. \n Includes 20 hours of video instruction, text book, course workbook, templates for getting started, and 4 weekly group coaching calls.',
+        'instructor': 'Luca Leyva',
+        'category': 'Agile',
+        'price': 499.99,
+        'countInStock': 100,
+        'rating': 4.5,
+        'numReviews': 12,
+    },
+    {
+        '_id': '2',
+        'name': 'Scrum',
+        'image': '/images/scrum.jpg',
+        'description':
+        'Learn how to use Scrum to deliver higher quality products that \n customers love at a quicker rate. \n Includes 10 hours of video instruction, text book, course workbook, templates for getting started, and 2 coaching.',
+        'instructor': 'Luca Leyva',
+        'category': 'Agile',
+        'price': 299.99,
+        'countInStock': 10,
+        'rating': 5.0,
+        'numReviews': 8,
+    },
+    {
+        '_id': '3',
+        'name': 'Cannon EOS 80D DSLR Camera',
+        'image': '/images/Lean.jpg',
+        'description':
+        'Learn how to use Lean to reduce waste, increse alignment and \n autonomy, and deliver high qulaity products as a much faster \n pace. \n Includes 20 hours of video instruction, text book, course workbook, templates for getting started, and 4 weekly group coaching calls.',
+        'instructor': 'Luca Leyva',
+        'category': 'Lean',
+        'price': 699.99,
+        'countInStock': 10,
+        'rating': 5.0,
+        'numReviews': 8,
+    },
+    {
+        '_id': '4',
+        'name': 'Applied Systems Thinking',
+        'image': '/images/systems_thinking.jpg',
+        'description':
+        'Learn how to see and understand the various systems at your workplace, how to influence systmic change, and how to train up other systems thinkers. Includes 3 months of live weekly coaching calls, a text book, course workbook, and templates for getting started.',
+        'instructor': 'Luca Leyva',
+        'category': 'Systems Thinking',
+        'price': 1299.99,
+        'countInStock': 10,
+        'rating': 5,
+        'numReviews': 12,
+    },
+    {
+        '_id': '5',
+        'name': 'Kanban',
+        'image': '/images/kanban.jpg',
+        'description':
+        'Learn how to visualize your work, identify the various "time theifs" and how to address them, and achieve a sustainable delivery pace. Includes a 10 hours of video, a text book, a course workbook, templates for getting started, and 2 coaching calls.',
+        'instructor': 'Luca Leyva',
+        'category': 'Kanban',
+        'price': 299.99,
+        'countInStock': 20,
+        'rating': 4.5,
+        'numReviews': 10,
+    },
+]
