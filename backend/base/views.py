91fd3d003b379fac8df23e0ad94304aa8202975c
Luca Leyva <imlucaleyva@gmail.com>
2021-12-06 14:29:19 -0500

    Created User Serializer. Customized JWT fields further--based off of Django User Model

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerailizerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -13,10 +13,10 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
         
-        data['username'] = self.user.username
-        data['email'] = self.user.email
-
-            
+        serializer = UserSerailizerWithToken(self.user).data
+        # k = key, v = value
+        for k, v in serializer.items():
+            data[k] = v
         return data
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -38,6 +38,13 @@ def getRoutes(request):
         '/api/products/<update>/<id>/',
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
