1074f98f1f34387487137aee9ea62cba7617672f
Luca Leyva <imlucaleyva@gmail.com>
2021-12-09 15:53:13 -0500

    Bug fixes related to the Order Screen.

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -6,11 +6,4 @@ urlpatterns = [
 
     path('add', views.addOrderItems, name='orders-add'),
     path('<str:pk>', views.getOrderById, name='user-order'),
-
-    # path('', views.getOrders, name='orders'),
-    # path('myorders', views.getMyOrders, name='myorders'),
-
-    # path('<str:pk>/deliver', views.updateOrderToDelivered, name='order-delivered'),
-
-    # path('<str:pk>/pay', views.updateOrderToPaid, name='pay'),
 ]
