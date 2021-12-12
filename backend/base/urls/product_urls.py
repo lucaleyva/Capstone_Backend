6912e6980b49b476af51903e2bbe88caaddc10ec
Luca Leyva <imlucaleyva@gmail.com>
2021-12-12 10:11:52 -0500

    Pushing up the unsaved changed for the create and update endpoints.

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     
     path('<str:pk>', views.getProduct, name="product"),
     
+    path('update/<str:pk>', views.updateProduct, name="product-update"),
+    
     path('delete/<str:pk>', views.deleteProduct, name="product-delete"),
 ] 
