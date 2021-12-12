d008bcc303fb87c36b78e3bb3d5a2a6c696f808c
Luca Leyva <imlucaleyva@gmail.com>
2021-12-12 11:47:37 -0500

    Added UpdateToDeliver endpoint

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('add', views.addOrderItems, name='orders-add'),
     path('myorders', views.getMyOrders, name='my-orders'),
     
+        path('<str:pk>/deliver', views.updateOrderToDelivered, name='order-delivered'),
+
+    
     path('<str:pk>', views.getOrderById, name='user-order'),
     path('<str:pk>/pay', views.updateOrderToPaid, name='pay'),
 ]
